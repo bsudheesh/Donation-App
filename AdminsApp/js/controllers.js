690e893c077fc92aadfe6e3f04fc65a5ce66f551
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2016-11-12 10:16:17 -0500

    added GUI for Admin Desltop Interface

---
+++ AdminsApp/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('causeFeedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('hUAdminLoginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('addCauseCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editCauseCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('feedDetailCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ AdminsApp/js/routes.js
@@ -0,0 +1,47 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('causeFeed', {
+    url: '/cause_feed',
+    templateUrl: 'templates/causeFeed.html',
+    controller: 'causeFeedCtrl'
+  })
+
+  .state('hUAdminLogin', {
+    url: '/Login',
+    templateUrl: 'templates/hUAdminLogin.html',
+    controller: 'hUAdminLoginCtrl'
+  })
+
+  .state('addCause', {
+    url: '/add_cause',
+    templateUrl: 'templates/addCause.html',
+    controller: 'addCauseCtrl'
+  })
+
+  .state('editCause', {
+    url: '/edit_cause',
+    templateUrl: 'templates/editCause.html',
+    controller: 'editCauseCtrl'
+  })
+
+  .state('feedDetail', {
+    url: '/detail',
+    templateUrl: 'templates/feedDetail.html',
+    controller: 'feedDetailCtrl'
+  })
+
+$urlRouterProvider.otherwise('/Login')
+
+  
+
+});
